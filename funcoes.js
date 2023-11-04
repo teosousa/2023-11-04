a388fd83843e0648c6b3eea2d0294652431f7c5b
Teo <teosousa1@gmail.com>
2023-11-04 16:31:54 -0300

    terminando aula dia 04

---
+++ funcoes.js
@@ -0,0 +1,26 @@
+// function nome(){
+
+//     let meuNome = "Teo";
+
+// alert(meuNome);
+
+// }
+// nome()
+
+// function multi5(exibirValor){
+
+// alert(exibirValor)
+
+// return exibirValor * 5;
+
+// }
+
+// alert("O valor do numero multiplicado por 5 é" + multi5(60))
+
+function soma(numero1,numero2,){
+
+    return numero1 + numero2;
+    
+    }
+
+    alert(soma(45,5))
